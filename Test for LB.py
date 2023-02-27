4e17b2b6dd83c078fb9446f572d019cbfe2a5489
IStrathdee <124535491+IStrathdee@users.noreply.github.com>
2023-02-27 16:07:30 +0000

    Update Test for LB.py

--- Test for LB.py
+++ Test for LB.py
@@ -1,6 +1,21 @@
-import numpy as np
-import pandas as pd
+import pandas as pd 
 import scipy
 
-df = pd.read_csv(r'/Users/Ishbels-MacBook-Air/Desktop/lglx_kbol_tuv09.csv')
-print(df)
+#begin with finding the bolometric corrected luminosities of the galaxies 
+#import the correction table and get python to read ecah column
+lxcorr_table = pd.read_csv('lglx_kbol_tuv09.csv')
+
+xp = lxcorr_table["LX_hard_corr"]
+#print(xp)
+fp = lxcorr_table["bolometric_correction"]
+#print(fp)
+
+lx_data_point = pd.read_csv('xL data points.csv')
+x = lx_data_point["Log(L)"]
+#pd.Series(['Log(L)']).convert_objects(convert_numeric=True)
+# x = float(x)
+print(x) 
+
+for i in x:
+  arr2 = np.interp(x, xp, fp)
+print (arr2) 
